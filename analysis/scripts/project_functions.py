381f04eded6de606c56bff236a110fbcb16a2119
brandonboscher <boscherbrandon@gmail.com>
2021-08-01 15:55:42 -0700

    pushing to update main branch with fires data

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -11,112 +11,115 @@ import numpy as py
 # In[2]:
 
 
-def load_and_process(file_path):
+def load_n_process(file_path):
 
-    #Chain 1: Loading Data and removing NaN rows.
+    #Chain 1: Loading Data, removing unnecessary columns, and renaming necessary columns.
     df1 = (
         pd.read_csv(file_path, encoding = 'ISO-8859-1', low_memory=False)
-        .dropna(inplace=True)
+        .drop(columns=['NWCG_REPORTING_UNIT_NAME', 'FPA_ID', 'SOURCE_SYSTEM_TYPE', 'SOURCE_SYSTEM', 'NWCG_REPORTING_AGENCY', 'NWCG_REPORTING_UNIT_ID', 'SOURCE_REPORTING_UNIT', 'SOURCE_REPORTING_UNIT_NAME', 'LOCAL_FIRE_REPORT_ID', 'LOCAL_INCIDENT_ID', 'FIRE_CODE', 'ICS_209_PLUS_INCIDENT_JOIN_ID', 'ICS_209_PLUS_COMPLEX_JOIN_ID', 'MTBS_ID', 'MTBS_FIRE_NAME', 'COMPLEX_NAME', 'DISCOVERY_DOY', 'NWCG_CAUSE_AGE_CATEGORY', 'OWNER_DESCR', 'COUNTY', 'NWCG_GENERAL_CAUSE'])
+        .rename(columns = {'NWCG_CAUSE_CLASSIFICATION':'CAUSE'})
+        .dropna()
     )
     
-    #Chain 2: Dropping unnecessary columns and rows, and resetting the index to aid in analysis.
+    df2 = df1
+    
+    #Chain 2: Dropping unnecessary rows, and resetting the index to aid in analysis.
     df2 = (
-        df1
-        .drop(columns=['NWCG_REPORTING_UNIT_NAME', 'FPA_ID', 'SOURCE_SYSTEM_TYPE', 'SOURCE_SYSTEM', 'NWCG_REPORTING_AGENCY', 'NWCG_REPORTING_UNIT_ID', 'SOURCE_REPORTING_UNIT', 'SOURCE_REPORTING_UNIT_NAME', 'LOCAL_FIRE_REPORT_ID', 'LOCAL_INCIDENT_ID', 'FIRE_CODE', 'ICS_209_PLUS_INCIDENT_JOIN_ID', 'ICS_209_PLUS_COMPLEX_JOIN_ID', 'MTBS_ID', 'MTBS_FIRE_NAME', 'COMPLEX_NAME', 'DISCOVERY_DOY', 'NWCG_CAUSE_AGE_CATEGORY', 'OWNER_DESCR', 'COUNTY', 'NWCG_GENERAL_CAUSE'], inplace=True)
-        .rename(columns = {'NWCG_CAUSE_CLASSIFICATION':'CAUSE'}, inplace = True)
-        .drop(df[(df['FIRE_SIZE_CLASS']=='A')].index, inplace=True)
-        .drop(df[(df['FIRE_SIZE_CLASS']=='B')].index, inplace=True)
-        .reset_index(inplace=True)
+        df2.drop(df2[(df2['FIRE_SIZE_CLASS']=='A')].index)
+        .drop(df2[(df2['FIRE_SIZE_CLASS']=='B')].index)
+        .reset_index()
+        
     )
 
-    dflatest=df2
-    return dflatest
+    df=df2
+    return df
+
 
 
 # In[ ]:
 
-def load_and_process_brandon(file_path):
+def load_and_process_brandon(filepath1,filepath2,filepath3):
+    #Chain 1: Dropping 
+    dff = pd.read_csv(filepath1, encoding = 'ISO-8859-1', low_memory=False)
+    dff = (
+        dff.drop(columns=["FOD_ID","FPA_ID","SOURCE_SYSTEM_TYPE","SOURCE_SYSTEM","NWCG_REPORTING_AGENCY","NWCG_REPORTING_UNIT_ID","NWCG_REPORTING_UNIT_NAME","SOURCE_REPORTING_UNIT","SOURCE_REPORTING_UNIT_NAME","LOCAL_FIRE_REPORT_ID","LOCAL_INCIDENT_ID","FIRE_CODE","FIRE_NAME","ICS_209_PLUS_INCIDENT_JOIN_ID","ICS_209_PLUS_COMPLEX_JOIN_ID","MTBS_ID","MTBS_FIRE_NAME","COMPLEX_NAME","DISCOVERY_DATE","DISCOVERY_DOY","DISCOVERY_TIME","NWCG_GENERAL_CAUSE","NWCG_CAUSE_AGE_CATEGORY","CONT_DATE","CONT_DOY","CONT_TIME","LATITUDE","LONGITUDE","OWNER_DESCR","COUNTY","FIPS_CODE","FIPS_NAME"])
+        .drop(dff[dff.NWCG_CAUSE_CLASSIFICATION == 'Human'].index)
+        .drop(dff[dff.NWCG_CAUSE_CLASSIFICATION == 'Missing data/not specified/undetermined'].index)
+        .dropna()
+        .drop(dff[(dff.FIRE_SIZE_CLASS == 'A')].index)
+        .drop(columns=['NWCG_CAUSE_CLASSIFICATION'])
+    )
 
-    dff = pd.read_csv("C:/Users/Don/Desktop/DATA 301/Project/FiresNEW.csv", encoding = 'ISO-8859-1', low_memory=False)
-dff = (
-    dff.drop(columns=["FOD_ID","FPA_ID","SOURCE_SYSTEM_TYPE","SOURCE_SYSTEM","NWCG_REPORTING_AGENCY","NWCG_REPORTING_UNIT_ID","NWCG_REPORTING_UNIT_NAME","SOURCE_REPORTING_UNIT","SOURCE_REPORTING_UNIT_NAME","LOCAL_FIRE_REPORT_ID","LOCAL_INCIDENT_ID","FIRE_CODE","FIRE_NAME","ICS_209_PLUS_INCIDENT_JOIN_ID","ICS_209_PLUS_COMPLEX_JOIN_ID","MTBS_ID","MTBS_FIRE_NAME","COMPLEX_NAME","DISCOVERY_DATE","DISCOVERY_DOY","DISCOVERY_TIME","NWCG_GENERAL_CAUSE","NWCG_CAUSE_AGE_CATEGORY","CONT_DATE","CONT_DOY","CONT_TIME","LATITUDE","LONGITUDE","OWNER_DESCR","COUNTY","FIPS_CODE","FIPS_NAME"])
-    .drop(dff[dff.NWCG_CAUSE_CLASSIFICATION == 'Human'].index)
-    .drop(dff[dff.NWCG_CAUSE_CLASSIFICATION == 'Missing data/not specified/undetermined'].index)
-    .dropna()
-    .drop(dff[(dff.FIRE_SIZE_CLASS == 'A')].index)
-    .drop(columns=['NWCG_CAUSE_CLASSIFICATION'])
-)
 
+    dfstate = pd.read_csv(filepath2)
+    dfstate = (
+        dfstate.drop(columns='Abbrev')
+        .rename(columns={'Code':'STATE'})
+    )
 
-dfstate = pd.read_csv("C:/Users/Don/Desktop/DATA 301/Project/csvData.csv")
-dfstate = (
-    dfstate.drop(columns='Abbrev')
-    .rename(columns={'Code':'STATE'})
-)
-
-df3 = (
-    df1.set_index('STATE').combine_first(dfstate.set_index('STATE'))
-    .reset_index(inplace=True)
-    .drop(columns='STATE')
-    .dropna()
-    .rename(columns={'FIRE_YEAR':'Date','State':'Location'})
-)
+    df3 = (
+        df1.set_index('STATE').combine_first(dfstate.set_index('STATE'))
+        .reset_index(inplace=True)
+        .drop(columns='STATE')
+        .dropna()
+        .rename(columns={'FIRE_YEAR':'Date','State':'Location'})
+    )
     df2['Date'] = df2['Date'].astype(int)
 
     
-dftemp=pd.read_csv("C:/Users/Don/Desktop/DATA 301/Project/tavg.csv")
-f = lambda dftemp : dftemp['Location'].split("CD")
-dftemp['Location'] = dftemp.apply(f, axis=1)
-g = lambda dftemp : dftemp['Location'].pop(0)
-dftemp['Location'] = dftemp.apply(g, axis=1)
-dftemp.reset_index()
-dftemp = dftemp.drop(columns=['Location ID','Rank','Anomaly (1901-2000 base period)','1901-2000 Mean'])
-dftemp['Location'] = dftemp['Location'].str.rstrip()
-dftemp['Celsius'] = (dftemp['Value']-32)*5/9
-dftemp['Celsius'] = dftemp['Celsius'].round(1)
-dftemp = dftemp.drop(columns=['Value'])
-dftemp = dftemp.drop(dftemp[dftemp['Date'] % 10 != 7].index)
-dftemp['Date'] = dftemp['Date']//100
-dftemp['Date'] = dftemp['Date'].astype(int)
-dftemp = dftemp.drop(dftemp[dftemp.Date > 2018].index)
-dftemp = dftemp.drop(dftemp[dftemp['Date'] < 1992].index)
-dftemp = dftemp.reset_index(drop = True)
-dftemp = dftemp.reset_index(drop = False)
+    dftemp=pd.read_csv(filepath3)
+    f = lambda dftemp : dftemp['Location'].split("CD")
+    dftemp['Location'] = dftemp.apply(f, axis=1)
+    g = lambda dftemp : dftemp['Location'].pop(0)
+    dftemp['Location'] = dftemp.apply(g, axis=1)
+    dftemp.reset_index()
+    dftemp = dftemp.drop(columns=['Location ID','Rank','Anomaly (1901-2000 base period)','1901-2000 Mean'])
+    dftemp['Location'] = dftemp['Location'].str.rstrip()
+    dftemp['Celsius'] = (dftemp['Value']-32)*5/9
+    dftemp['Celsius'] = dftemp['Celsius'].round(1)
+    dftemp = dftemp.drop(columns=['Value'])
+    dftemp = dftemp.drop(dftemp[dftemp['Date'] % 10 != 7].index)
+    dftemp['Date'] = dftemp['Date']//100
+    dftemp['Date'] = dftemp['Date'].astype(int)
+    dftemp = dftemp.drop(dftemp[dftemp.Date > 2018].index)
+    dftemp = dftemp.drop(dftemp[dftemp['Date'] < 1992].index)
+    dftemp = dftemp.reset_index(drop = True)
+    dftemp = dftemp.reset_index(drop = False)
 
 
 
-dffinal = (
-    df2.groupby(['Date','Location']).size()
-    .to_frame()
-    .reset_index(inplace = True)
-    .drop(dffinal[dffinal.Location == 'Alaska'].index)
-    .drop(dffinal[dffinal.Location == 'Hawaii'].index)
-    .sort_values(by=['Location','Date'])
-    .rename(columns={0: '# of Fires'})
-    .reset_index(drop=True)
-    .reset_index(drop=False)
-)
+    dffinal = (
+        df2.groupby(['Date','Location']).size()
+        .to_frame()
+        .reset_index(inplace = True)
+        .drop(dffinal[dffinal.Location == 'Alaska'].index)
+        .drop(dffinal[dffinal.Location == 'Hawaii'].index)
+        .sort_values(by=['Location','Date'])
+        .rename(columns={0: '# of Fires'})
+        .reset_index(drop=True)
+        .reset_index(drop=False)
+    )
 
-d1=dffinal
-d1 = d1.drop(columns=['index'])
-d1['Date']=d1['Date'].values.astype(str)
-d1['Location']=d1['Location'].values.astype(str)
-d1['id'] = d1['Date'].str.cat(d1['Location'],sep='-')
-d1
+    d1=dffinal
+    d1 = d1.drop(columns=['index'])
+    d1['Date']=d1['Date'].values.astype(str)
+    d1['Location']=d1['Location'].values.astype(str)
+    d1['id'] = d1['Date'].str.cat(d1['Location'],sep='-')
+    d1
 
 
-dftemp12 = dftemp
-dftemp12 = dftemp12.drop(columns=['index'])
-dftemp12['Date']=dftemp12['Date'].values.astype(str)
-dftemp12['Location']=dftemp12['Location'].values.astype(str)
-dftemp12['id'] = dftemp12['Date'].str.cat(dftemp12['Location'],sep='-')
-dftemp100 = dftemp12.drop(columns=['Date','Location'])
-dftemp100
+    dftemp12 = dftemp
+    dftemp12 = dftemp12.drop(columns=['index'])
+    dftemp12['Date']=dftemp12['Date'].values.astype(str)
+    dftemp12['Location']=dftemp12['Location'].values.astype(str)
+    dftemp12['id'] = dftemp12['Date'].str.cat(dftemp12['Location'],sep='-')
+    dftemp100 = dftemp12.drop(columns=['Date','Location'])
+    dftemp100
 
-dffinal = pd.merge(d1,dftemp100,how='right',on='id')
-dffinal = dffinal.drop(columns=['id'])
+    dffinal = pd.merge(d1,dftemp100,how='right',on='id')
+    dffinal = dffinal.drop(columns=['id'])
 
-return dffinal
+    return dffinal
 
 
 
